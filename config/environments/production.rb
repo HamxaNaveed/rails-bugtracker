1b5415a4b7ce329c0e37d9d82e7578184ff4b6fa
Hamza Naveed <officialhamxa@gmail.com>
2022-10-20 12:15:34 +0500

    Seed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,5 +89,13 @@ Rails.application.configure do
   end
 
   # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
+    config.active_record.dump_schema_after_migration = false
+
+    config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
+end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+roles = [
+    {name: "manager"},
+    {name: "developer"},
+    {name: "qa"},
+]
